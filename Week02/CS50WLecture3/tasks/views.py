42155ddee5e295435d998962bfd3729657bee349
David Lightfoot <dslightfoot@yahoo.com>
2024-04-14 06:19:48 -0400

    Working add task button with redirect to /tasks

--- Week02/CS50WLecture3/tasks/views.py
+++ Week02/CS50WLecture3/tasks/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django import forms
-
-
-class NewTaskForm(forms.Form):
-    task = forms.CharField(label="New Task")
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
-tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task") 
+
+tasks = []
 
 def index(request):
     return render(request, "tasks/index.html",{
@@ -14,6 +15,16 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "task/add.html",{
+                "form": form
+            })
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
