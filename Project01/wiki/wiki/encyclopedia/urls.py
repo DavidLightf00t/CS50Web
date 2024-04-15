5074ddc6b379f58f2758bea15d3a08d2bd6728f3
David Lightfoot <dslightfoot@yahoo.com>
2024-04-15 00:49:45 -0400

    Working pages for /wiki/PAGE

--- Project01/wiki/wiki/encyclopedia/urls.py
+++ Project01/wiki/wiki/encyclopedia/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new", views.new, name="new")
+    path("new", views.new, name="new"),
+    path("wiki/<str:title>", views.entry, name="entry"),
 ]
--- Project01/wiki/wiki/encyclopedia/views.py
+++ Project01/wiki/wiki/encyclopedia/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 
 def index(request):
@@ -13,3 +22,14 @@ def new(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html", {
+            "error": "This entry does not exist"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content,
+        })
