dfeb2b092bbef571bc501ad192be0cdace60da1f
David Lightfoot <dslightfoot@yahoo.com>
2024-04-15 01:26:56 -0400

    Made working search bar with links

--- Project01/wiki/wiki/encyclopedia/views.py
+++ Project01/wiki/wiki/encyclopedia/views.py
@@ -33,3 +33,20 @@ def entry(request, title):
             "title": title,
             "content": html_content,
         })
+
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html = convert_md_to_html(entry_search)
+
+        if html is not None:
+            return render(request, "encyclopedia/entry.html",{
+                "title": entry_search,
+                "content": html
+            })
+        else:
+            return render(request, "encyclopedia/error.html",{
+                "error": "This entry does not exist"
+            })
+
+    
